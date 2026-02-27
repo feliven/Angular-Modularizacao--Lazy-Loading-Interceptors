7c7429f9ca16d57978a48d73f15518820fa53b31
FVS <feliven@gmail.com>
2026-02-27 13:21:31 -0300

    implementa lazy loading para componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
-import { BuscaComponent } from './pages/busca/busca.component';
 import { authGuard } from './core/guards/auth.guard';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
@@ -14,20 +10,41 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    component: LoginComponent,
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'cadastro',
-    component: CadastroComponent,
+    loadComponent: () =>
+      import('./pages/cadastro/cadastro.component').then(
+        (m) => m.CadastroComponent,
+      ),
   },
   {
     path: 'perfil',
-    component: PerfilComponent,
+    loadComponent: () =>
+      import('./pages/perfil/perfil.component').then((m) => m.PerfilComponent),
     canActivate: [authGuard],
   },
   {
     path: 'busca',
-    component: BuscaComponent,
+    loadComponent: () =>
+      import('./pages/busca/busca.component').then((m) => m.BuscaComponent),
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./core/erro/erro.module').then((m) => m.ErroModule),
+  },
+  {
+    path: '**',
+    redirectTo: '/pagina-nao-encontrada',
+    pathMatch: 'full',
   },
 ];
 
