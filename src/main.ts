afb3eeb189d7e5af5c5ce5806bd8a378d71a0c10
FVS-MBA <feliven@gmail.com>
2026-02-26 21:08:41 -0300

    corrige comportamento do Perfil ao chamar httpclient com autenticação

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,23 @@ import {
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import routes from './app/app-routing.module';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { AutenticacaoInterceptor } from './app/core/interceptors/autenticacao.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AutenticacaoInterceptor,
+      multi: true,
+    },
     provideCheckNoChangesConfig({ exhaustive: true, interval: 1000 }),
   ],
 });
